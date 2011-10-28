4e12c4fb771eeb42fc7f48526c473d84a6969497
Nick Felicelli <nick.felicelli@gmail.com>
2011-10-28 12:59:02 -0400

    Ignoring the application.ini Updated the distribution ini Created the routes config with auth and page routes Update Bootstrap to remove default routes

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -2,7 +2,14 @@
 
 class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
 {
-
-
+    protected function _initRoutes()
+    {
+        // remove the default routes
+        $router = Zend_Controller_Front::getInstance()->getRouter();
+        $router->removeDefaultRoutes();
+        
+        return $router;
+    }
+        
 }
 
