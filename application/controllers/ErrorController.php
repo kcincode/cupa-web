d9d8d4589a0facbff8e6e2ce634b22b641079c47
Nick Felicelli <nick.felicelli@gmail.com>
2012-04-11 19:35:58 +0000

    Created the 404 error page.

--- application/controllers/ErrorController.php
+++ application/controllers/ErrorController.php
@@ -2,16 +2,21 @@
 
 class ErrorController extends Zend_Controller_Action
 {
+    public function init()
+    {
+        // change the layout file for all pages.
+        $this->_helper->_layout->setLayout('error');
+    }
 
     public function errorAction()
     {
         $errors = $this->_getParam('error_handler');
-        
+
         if (!$errors || !$errors instanceof ArrayObject) {
             $this->view->message = 'You have reached the error page';
             return;
         }
-        
+
         switch ($errors->type) {
             case Zend_Controller_Plugin_ErrorHandler::EXCEPTION_NO_ROUTE:
             case Zend_Controller_Plugin_ErrorHandler::EXCEPTION_NO_CONTROLLER:
@@ -20,26 +25,28 @@ class ErrorController extends Zend_Controller_Action
                 $this->getResponse()->setHttpResponseCode(404);
                 $priority = Zend_Log::NOTICE;
                 $this->view->message = 'Page not found';
+                $this->_forward('notfound');
                 break;
             default:
                 // application error
                 $this->getResponse()->setHttpResponseCode(500);
                 $priority = Zend_Log::CRIT;
                 $this->view->message = 'Application error';
+                $this->_forward('server');
                 break;
         }
-        
+
         // Log exception, if logger available
         if ($log = $this->getLog()) {
             $log->log($this->view->message, $priority, $errors->exception);
             $log->log('Request Parameters', $priority, $errors->request->getParams());
         }
-        
+
         // conditionally display exceptions
         if ($this->getInvokeArg('displayExceptions') == true) {
             $this->view->exception = $errors->exception;
         }
-        
+
         $this->view->request   = $errors->request;
     }
 
@@ -55,17 +62,12 @@ class ErrorController extends Zend_Controller_Action
 
     public function notfoundAction()
     {
-        // action body
+        $this->view->headLink()->appendStylesheet($this->view->baseUrl() . '/css/error/notfound.css');
     }
 
     public function serverAction()
     {
-        // action body
-    }
-
-    public function notauthorizedAction()
-    {
-        // action body
+        $this->view->headLink()->appendStylesheet($this->view->baseUrl() . '/css/error/server.css');
     }
 
 
