039213ad64c48720b6a5d2fd3534bbbdee7ce122
Nick Felicelli <nick.felicelli@gmail.com>
2012-07-13 16:32:40 -0400

    Adding missing files from previous commit

---
+++ application/views/helpers/DisplayClubRostersLink.php
@@ -0,0 +1,27 @@
+<?php
+
+class My_View_Helper_DisplayClubRostersLink extends Zend_View_Helper_Abstract
+{
+    public $view;
+ 
+    public function setView(Zend_View_Interface $view)
+    {
+        $this->view = $view;
+    }    
+    
+    public function displayClubRostersLink($clubId)
+    {
+        if(is_numeric($clubId)) {
+            $clubMemberTable = new Model_DbTable_ClubMember();
+            $members = $clubMemberTable->fetchAllMemberByYear($clubId);
+            if(count($members)) {
+                $keys = array_keys($members);
+                $year = $keys[sizeof($keys) - 1];
+                return '<a class="button roster" href="' . $this->view->url(array('club_id' => $clubId), 'club_home') . '#' . $year . '">View Rosters</a>';
+            }            
+            return '';
+        }
+
+        return '';
+    }
+}
