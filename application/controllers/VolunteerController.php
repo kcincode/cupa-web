1cd4a454b88c9cec6189bf93daa2a75727be2a30
Nick Felicelli <nick.felicelli@gmail.com>
2012-07-26 16:03:22 -0400

    Added volunteer info page and stubs for registering and viewing volunteer opportunities

--- application/controllers/VolunteerController.php
+++ application/controllers/VolunteerController.php
@@ -4,9 +4,21 @@ class VolunteerController extends Zend_Controller_Action
 {
     public function init()
     {
+        $this->view->headLink()->appendStylesheet($this->view->baseUrl() . '/css/page/view.css');
     }
 
     public function indexAction()
     {
+        $this->view->headLink()->appendStylesheet($this->view->baseUrl() . '/css/volunteer/index.css');
+    }
+
+    public function registerAction()
+    {
+
+    }
+
+    public function listAction()
+    {
+
     }
 }
