fb984153363405e3d86a3db1c22996a0eef33441
Nick Felicelli <kcin1018@gmail.com>
2013-01-25 11:21:08 -0500

    Removing not needed files

--- public/js/page/minutesedit.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-    $('#when').datetimepicker({
-        dateFormat: 'yy-mm-dd',
-        timeFormat: 'hh:mm:ss',
-        changeMonth: true,
-        changeYear: true
-    });
-});
