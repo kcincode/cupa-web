399c231304c0b487b484ef7380fa28e8616452e1
Nick Felicelli <nick.felicelli@gmail.com>
2013-03-27 13:25:09 -0400

    Added validation to league archive.

--- application/controllers/LeagueController.php
+++ application/controllers/LeagueController.php
@@ -210,11 +210,16 @@ class LeagueController extends Zend_Controller_Action
         $leagueTable = new Model_DbTable_League();
         $leagueSeasonTable = new Model_DbTable_LeagueSeason();
 
+        $season = $leagueSeasonTable->fetchName($league->season);
+
+        if($this->view->isLeagueDirector($leagueId)) {
+            $this->_redirect('leagues/' . $season . '/' . $this->view->slugify($this->view->leaguename($leagueId, true, false, false, true)));
+        }
+
         $league = $leagueTable->find($leagueId)->current();
         $league->is_archived = ($league->is_archived == 1) ? 0 : 1;
         $league->save();
 
-        $season = $leagueSeasonTable->fetchName($league->season);
         $this->_redirect('leagues/' . $season . '/' . $this->view->slugify($this->view->leaguename($league->id, true, false, false, true)));
     }
 
